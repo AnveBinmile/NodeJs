c6fb815305c77f4362b3fad8c39d2ead34d4aca7
anvesha <anvesha@binmile.com>
2023-10-10 11:15:22 +0530

    multer

--- Day2/MoveFile/index.js
+++ Day2/MoveFile/index.js
@@ -8,29 +8,35 @@ const port = 7000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/", multer().single("file"), (req, res) => {
-  const fileNameData = req.file.originalname.split(".");
+const extractFileData = (data, folder, res) => {
+  const fileNameData = data.originalname.split(".");
   const fileName = fileNameData[0];
   const fileExtension = fileNameData[1];
   const date = new Date();
-
   const newFileName =
     fileName + date.getTime().toString() + "." + fileExtension;
-  const size = req.file.size / 1000;
-  req.file.originalname = newFileName;
+  const size = data.size / 1000000;
+  data.originalname = newFileName;
+  fs.writeFileSync(`./uploads/${folder}/${newFileName}`, data.buffer);
 
-  if (size >= 30) {
+  if (folder === "images" && size >= 5) {
+    res.send(`${size}MB EXCEEDS EXCEEDS FILE SIZE LIMIT`);
+  } else if (folder === "pdfs" && size >= 15) {
     res.send(`${size}MB EXCEEDS EXCEEDS FILE SIZE LIMIT`);
   }
+};
+
+app.post("/", multer().single("file"), (req, res) => {
+  const fileExtension = req.file.originalname.split(".")[1];
 
   if (
     fileExtension === "png" ||
     fileExtension === "jpg" ||
     fileExtension === "jpeg"
   ) {
-    fs.writeFileSync(`./uploads/images/${newFileName}`, req.file.buffer);
+    extractFileData(req.file, "images", res);
   } else {
-    fs.writeFileSync(`./uploads/pdfs/${newFileName}`, req.file.buffer);
+    extractFileData(req.file, "pdfs", res);
   }
 
   res.send("UPLOAD SUCCESS");
